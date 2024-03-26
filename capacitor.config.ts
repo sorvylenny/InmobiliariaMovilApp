f6d3c319ca5145be518517854894b281da79f32a
sorvylenny <floresmkatherine@gmail.com>
2024-03-26 16:44:09 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'InmobiliariaFloresMApp',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
